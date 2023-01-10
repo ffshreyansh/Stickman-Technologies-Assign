18d3ea90e753060607fcb95abe2fb4e73ee19045
Shreyansh Kumar <shreyanshkumar058@gmail.com>
2023-01-11 00:45:48 +0530

    mongoDb to mongoose

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 const MongoClient = require('mongodb').MongoClient;
 let token = 10000;
 app.use(bodyParser.urlencoded({ extended: false }));
-const uri = process.env.MONGO_URL ||"mongodb+srv://stickman:shreyansh@stickman.jtwgqqr.mongodb.net/?retryWrites=true&w=majority";
+const uri = process.env.MONGO_URL ||" mongodb+srv://stickman:shreyansh@stickman.jtwgqqr.mongodb.net/?retryWrites=true&w=majority"
 
 
 
@@ -67,149 +67,138 @@ app.use((req, res, next) => {
 app.use('/', require("./routes/index.js"));
 app.use('/users', require("./routes/users.js"));
 
-
+//Changing var name
+const mongooseConnection = mongoose.connection;
 //Pushing the Data into Mongo DB ====================================================================
 
-MongoClient.connect(uri, { useNewUrlParser: true }, function (err, client) {
+mongooseConnection.on('error', console.error.bind(console, 'connection error:'));
+mongooseConnection.once('open', function() {
+  console.log('Connected to MongoDB Atlas...');
+});
+const userSchema = new mongoose.Schema({
+  name: String,
+  number: [String]
+});
+
+const User1 = mongoose.model('User1', userSchema);
+
+app.post('/', function (req, res) {
+
+  const name = req.body.name;
+  const number = req.body.number;
+  const data = number.split(',');
+
+  let num = [];
+  data.forEach(number => {
+    num.push(number + " - " + token++);
+  });
+
+  User1.findOne({ name: name }, function (err, result) {
     if (err) {
-        console.log('Error occurred while connecting to MongoDB Atlas...\n', err);
-    }else if(typeof client !== 'undefined') {
-      console.log('Connected...');
-      const db = client.db('stickman');
+      console.log(err);
+    }
 
+    if (result) {
+      res.send("Data Already Exists with the Same Name!");
+    } else {
 
-    app.post('/', function (req, res) {
+      // Insert the data into the database
+      const user = new User1({ name: name, number: num });
+      user.save((err, result) => {
+        if (err) {
+          console.log(err);
+        }
+        console.log('Data added to the collection');
+        res.redirect('/success');
+      });
+    }
+  });
 
-        const name = req.body.name;
-        const number = req.body.number;
-        const data = number.split(',');
-
-        let num = [];
-        data.forEach(number => {
-            num.push(number + " - " + token++);
-        });
-
-        db.collection('users').findOne({ name: name }, function (err, result) {
-            if (err) {
-                console.log(err);
-            }
-            // If a document with the same name already exists, return an error
-            if (result) {
-                res.send("Data Already Exists with the Same Name!");
-            } else {
-
-                // Insert the data into the database
-                db.collection('users').insertOne({ name: name, number: num }, function (err, result) {
-                    if (err) {
-                        console.log(err);
-                    }
-                    console.log('Data added to the collection');
-                    res.redirect('/success');
-                });
-            }
-        });
-
-    });
-  }else {
-    console.log("client object is not defined")
-  }
-        //Using Puppeteer to Generate Name Filtered PDF=====================================
-
-        app.post('/pdf/filtered', async (req, res) => {
-            // Get the filter criteria from the query parameter
-            const filter = {name: req.body.name};
-          
-            // Find the documents that match the filter criteria
-            const collection = db.collection('users');
-            const data = await collection.find(filter).toArray();
-          
-            // Generate the PDF as before
-            const html = `
-              <html>
-                <body>
-                  <h1>Data from the database</h1>
-                  <table>
-                    <thead>
-                      <tr>
-                        <th>Name</th>
-                        <th>Number</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      ${data.map(row => `
-                        <tr>
-                          <td>${row.name}</td>
-                          <td>${row.number}</td>
-                        </tr>
-                      `).join('')}
-                    </tbody>
-                  </table>
-                </body>
-              </html>
-            `;
-            const browser = await puppeteer.launch();
-            const page = await browser.newPage();
-            await page.setContent(html);
-            const buffer = await page.pdf({ format: 'A4' });
-            await browser.close();
-          
-            // Send the PDF to the client
-            res.setHeader('Content-Type', 'stickman/pdf');
-            res.setHeader('Content-Disposition', 'attachment; filename="stickman.pdf"');    
-            res.send(buffer);
-          });
-          
+  app.post('/pdf/filtered', async (req, res) => {
+    // Get the filter criteria from the query parameter
+    const filter = {name: req.body.name};
+  
+    // Find the documents that match the filter criteria
+    const data = await User1.find(filter);
+  
+    // Generate the PDF as before
+    const html = `
+      <html>
+        <body>
+          <h1>Data from the database</h1>
+          <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Number</th>
+              </tr>
+            </thead>
+            <tbody>
+              ${data.map(row => `
+                <tr>
+                   <td>${row.name}</td>
+                   <td>${row.number}</td>
+                </tr>
+              `).join('')}
+            </tbody>
+          </table>
+        </body>
+      </html>
+    `;
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.setContent(html);
+    const buffer = await page.pdf({ format: 'A4' });
+    await browser.close();
+  
+    // Send the PDF to the client
+    res.setHeader('Content-Type', 'stickman/pdf');
+    res.setHeader('Content-Disposition', 'attachment; filename="stickman.pdf"');    
+    res.send(buffer);
+  });
 
 
 
         // Using Puppeteer for Admin's PDF generation==================================================================
 
         app.post('/pdf/admin', async (req, res) => {
-
-            const collection = db.collection('users');
-            // const filter = { name: 'c' };
-            // const data = await collection.find(filter).toArray();
-            const data = await collection.find().toArray();
-
-            //HTML for the PDF
-            const html = `
-  <html>
-    <body>
-      <h1>Data from the database</h1>
-      <table>
-        <thead>
+          // Find all documents in the collection
+          const data = await User1.find();
+          // Generating HTML for the PDF
+          const html = `
+<html>
+  <body>
+    <h1>Data from the database</h1>
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Number</th>
+        </tr>
+      </thead>
+      <tbody>
+        ${data.map(row => `
           <tr>
-            <th>Name</th>
-            <th>Number</th>
+            <td>${row.name}</td>
+            <td>${row.number}</td>
           </tr>
-        </thead>
-        <tbody>
-          ${data.map(row => `
-            <tr>
-              <td>${row.name}</td>
-              <td>${row.number}</td>
-            </tr>
-          `).join('')}
-        </tbody>
-      </table>
-    </body>
-  </html>
+        `).join('')}
+      </tbody>
+    </table>
+  </body>
+</html>
 `;
-
-            //Generating PDF
-            const browser = await puppeteer.launch();
-            const page = await browser.newPage();
-            await page.setContent(html);
-            const buffer = await page.pdf({ format: 'A4' });
-            await browser.close();
-
-            //Send the PDF to the Client
-            res.setHeader('Content-Type', 'stickmanAdmin/pdf');
-            res.setHeader('Content-Disposition', 'attachment; filename="stickmanAdmin.pdf"');
-            res.send(buffer);
-
-        });
-
+          // Generating PDF
+          const browser = await puppeteer.launch();
+          const page = await browser.newPage();
+          await page.setContent(html);
+          const buffer = await page.pdf({ format: 'A4' });
+          await browser.close();
+          // Send the PDF to the client
+          res.setHeader('Content-Type', 'stickmanAdmin/pdf');
+          res.setHeader('Content-Disposition', 'attachment; filename="stickmanAdmin.pdf"');
+          res.send(buffer);
+      });
 
     
 
