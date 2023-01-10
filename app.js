26c69bd0847c7a4f1690d115681cf1e04429a396
Shreyansh Kumar <shreyanshkumar058@gmail.com>
2023-01-10 18:24:52 +0530

    ..sd

--- app.js
+++ app.js
@@ -33,6 +33,39 @@ mongoose.connect(db, {useNewUrlParser: true})
     .then(() => console.log("MongoDB connected.."))
     .catch(err => console.log(err));
     
+//EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+//BodyParse
+app.use(express.urlencoded({extended: true}))
+
+//Express Session
+app.use(session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true
+}))
+
+//Passport middleware
+app.use(passport.initialize());
+app.use(passport.session()); 
+
+//flash
+app.use(flash());
+
+//Global Variables
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash("success_msg");
+    res.locals.error_msg = req.flash("error_msg");
+    res.locals.error = req.flash("error");
+    next();
+});
+
+
+//Routes
+app.use('/', require("./routes/index.js"));
+app.use('/users', require("./routes/users.js"));
 
 
 //Pushing the Data into Mongo DB ====================================================================
@@ -192,48 +225,6 @@ MongoClient.connect(uri, { useNewUrlParser: true }, function (err, client) {
     // app.get('/', (req, res) => {
     //     res.sendFile(__dirname + "/dashboard.ejs")
     // })
-
-
-
-
-
-
-
-
-//EJS
-app.use(expressLayouts);
-app.set('view engine', 'ejs');
-
-//BodyParse
-app.use(express.urlencoded({extended: true}))
-
-//Express Session
-app.use(session({
-    secret: "secret",
-    resave: true,
-    saveUninitialized: true
-}))
-
-//Passport middleware
-app.use(passport.initialize());
-app.use(passport.session()); 
-
-//flash
-app.use(flash());
-
-//Global Variables
-app.use((req, res, next) => {
-    res.locals.success_msg = req.flash("success_msg");
-    res.locals.error_msg = req.flash("error_msg");
-    res.locals.error = req.flash("error");
-    next();
-});
-
-
-//Routes
-app.use('/', require("./routes/index.js"));
-app.use('/users', require("./routes/users.js"));
-
 
 
 
